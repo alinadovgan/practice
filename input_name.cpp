4c80d58751ef059e11b17051a87b59fcc13a6add
alinadovgan <alinadovgan20@gmail.com>
2026-01-29 12:50:26 -0500

    Add user interactive

---
+++ input_name.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+
+    cout << "Enter Your name: ";
+    cin >> name; //read and store what user puts in string name
+
+    cout << "Hi " << name << "!" << endl;
+
+    return 0;
+}
